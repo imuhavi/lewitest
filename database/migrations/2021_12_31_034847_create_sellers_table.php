10537955348c417c70d79c3235182def3f26a86d
sumuhridoy2002 <sumuhridoy2002@gmail.com>
2021-12-31 18:22:58 +0600

    Migrate file for setting and seller

--- database/migrations/2021_12_31_034847_create_sellers_table.php
+++ database/migrations/2021_12_31_034847_create_sellers_table.php
@@ -15,6 +15,16 @@ class CreateSellersTable extends Migration
     {
         Schema::create('sellers', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('shop_name')->nullable();
+            $table->string('primary_phone')->nullable();
+            $table->string('secondary_phone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('shop_location')->nullable();
+            $table->string('shop_address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('country')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2021_12_31_120832_create_general_settings_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGeneralSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('general_settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('app_name')->nullable();
+            $table->string('app_logo_white')->nullable();
+            $table->string('app_logo_black')->nullable();
+            $table->string('app_phone')->nullable();
+            $table->string('app_email')->nullable();
+            $table->string('app_address')->nullable();
+            $table->string('app_copyright_text')->nullable();
+            $table->string('seo_title')->nullable();
+            $table->text('seo_description')->nullable();
+            $table->text('seo_keywords')->nullable();
+            $table->enum('mail_type', ['SMTP', 'GMAIL'])->nullable();
+            $table->string('mail_host')->nullable();
+            $table->string('mail_port')->nullable();
+            $table->string('mail_username')->nullable();
+            $table->string('mail_password')->nullable();
+            $table->string('mail_encryption')->nullable();
+            $table->string('mail_address')->nullable();
+            $table->string('mail_from_name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('general_settings');
+    }
+}
