302731085c24cfdd41332775bc10d0892bba586a
sumuhridoy2002 <sumuhridoy2002@gmail.com>
2022-01-04 13:02:34 +0600

    Working on withdraw confirmation api

---
+++ database/seeders/SocialConfigTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\SocialConfig;
+use Illuminate\Database\Seeder;
+
+class SocialConfigTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        SocialConfig::create([
+            'type' => 'Facebook',
+            'app_id' => '12345',
+            'app_secret' => '12345'
+        ]);
+        SocialConfig::create([
+            'type' => 'Google',
+            'app_id' => '12345',
+            'app_secret' => '12345'
+        ]);
+        SocialConfig::create([
+            'type' => 'Twitter',
+            'app_id' => '12345',
+            'app_secret' => '12345'
+        ]);
+    }
+}
