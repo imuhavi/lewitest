06a770572aeb756fcb11dd2dbf411c720a2b1873
sumuhridoy2002 <sumuhridoy2002@gmail.com>
2022-05-21 14:06:00 +0600

    Mobile & desctop navbar design - Done

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -14,8 +14,8 @@ use Illuminate\Http\Request;
 class FrontendController extends Controller
 {
   private $VIEW_PATH = 'frontend.frontend';
-  function frontend()
-  {
+  
+  function frontend(){
     $slider = Slider::where('status', 'Active')->get();
     $womensSub1 = Subcategory::where('category_id', 2)->get()->slice(0, 2);
     $womensSub2 = Subcategory::where('category_id', 2)->get()->slice(2, 4);
@@ -24,51 +24,41 @@ class FrontendController extends Controller
     $mensMain = Category::where('slug', 'mens')->where('status', 'Active')->first();
     // $womensMain = Category::where('slug', 'womens')->where('status', 'Active')->first();
     $accesoriesMain = Category::where('slug', 'accessories')->where('status', 'Active')->first();
-
     $categories = Category::whereIn('slug', ['mens', 'womens', 'accessories'])->whereStatus('Active')->take(3)->get();
     $womensMain = $categories->where('slug', 'womens')->first();
     $accesoriesSub = Subcategory::where('category_id', 4)->get()->slice(0, 4);
-
-  {
     return view('frontend.pages.shop');
   }
 
-  function singleProductView($slug)
-  {
+  function singleProductView($slug){
     return view('frontend.pages.productView');
   }
 
 
-  function cart()
-  {
+  function cart(){
     return view('frontend.pages.cart');
   }
 
-  function wishlist()
-  {
+  function wishlist(){
     return view('frontend.pages.wishlist');
   }
 
-  function checkout()
-  {
+  function checkout(){
     return view('frontend.pages.checkout');
   }
 
-  function subscription()
-  {
+  function subscription(){
     $data = Subscription::with('subscriptionOptions')->get();
     return view('frontend.pages.subscription', compact('data'));
   }
 
-  function getCity($stateId)
-  {
+  function getCity($stateId){
     // return Cities::all();
     $city = Cities::where('state_id', $stateId)->get();
     return response()->json($city);
   }
 
-  function sellerRegister(Subscription $subscription)
-  {
+  function sellerRegister(Subscription $subscription){
     $sates = States::get();
     return view('frontend.pages.sellerRegister', compact('subscription', 'sates'));
   }
