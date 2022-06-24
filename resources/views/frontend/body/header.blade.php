fa112cfac254c0751835605e372af7a4cadd416a
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-06-24 22:17:36 +0300

    update site title

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -2,7 +2,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>5dots Multi-vendor E-commerces</title>
+  <title>5dots</title>
   {{-- Ajax Request Meta --}}
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <script src="https://kit.fontawesome.com/b868f71921.js" crossorigin="anonymous"></script>
