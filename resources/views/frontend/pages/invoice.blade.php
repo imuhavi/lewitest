14bb0a666f07f03f889fcebed878bd4134824fd9
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-06-24 10:41:03 +0300

    create invoice

--- resources/views/frontend/pages/invoice.blade.php
+++ resources/views/frontend/pages/invoice.blade.php
@@ -6,15 +6,95 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Invoice</title>
-  <!-- Font Awesome Icon -->
-  <script src="https://kit.fontawesome.com/b868f71921.js" crossorigin="anonymous"></script>
-  <!-- Bootstrap Css link -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
   <style>
-    @font-face {
-      font-family: "five-dots-bold";
-      src: url(font/ArbFONTS-Luxury-1.0.otf);
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+      margin: 0;
+    }
+
+    ul,
+    ol,
+    ul li,
+    p {
+      list-style: none;
+      padding: 0;
+      margin: 0;
+    }
+
+    .container {
+      width: 1220px;
+      margin: 0 auto;
+    }
+
+    .hedaer {
+      padding: 15px 0px;
+      background-color: rgb(108, 108, 108);
+      border-radius: 10px;
+    }
+
+    .hedaer h3 {
+      text-align: center;
+      color: #fff;
+      font-size: 20px;
+      text-transform: uppercase;
+    }
+
+    .row {
+      display: flex;
+      justify-content: space-between;
+      margin-top: 30px;
+    }
+
+    .invoice-to ul li {
+      padding: 2px 0;
+    }
+
+    .invoce-no ul li {
+      padding: 2px 0;
+      text-align: right;
+    }
+
+    .table-dark {
+      background-color: black;
+      color: #fff;
+
+    }
+
+    .col-12,
+    table {
+      width: 100%;
+      overflow-x: scroll;
+    }
+
+    table th {
+      padding: 15px 10px;
+
+    }
+
+    .table-light {
+      background: rgb(232, 232, 232);
+      margin-top: 20px;
+    }
+
+    .table-light tr td {
+      padding: 10px;
+      text-align: center;
+    }
+
+    .calculation {
+      width: 100%;
+      display: flex;
+      justify-content: flex-end;
+    }
+
+    .calculation tr td {
+      padding: 5px;
+      text-align: right;
     }
   </style>
 
@@ -23,39 +103,56 @@
 <body>
   <header>
     <div class="container">
-      <!-- Company Logo -->
-      <nav class="text-center">
-        <a class="navbar-brand" href="../index.html"><span class="logo">Five
-            Dots</span></a>
-      </nav>
-
-      <div class="bg-secondary p-2 mt-3 text-center rounded">
-        <h3 class="text-white text-uppercase">Invoice</h3>
+      <div class="hedaer">
+        <h3>Invoice</h3>
       </div>
     </div>
   </header>
   <main>
-    <section class="container mt-3">
+    <section class="container">
+      {{ $order }}
       <div class="row">
-        <div class="col-6">
-          <div class="invoice-to">
-            <h3>Invoice to:</h3>
-            <h5>Rasel Ahmmed</h5>
-            <small>Phone: 572890762</small>
-            <br>
-            <small>Dammam, Khubar</small>
-            <address>
-              <small>123, King Abdulaziz Road.</small>
-            </address>
-          </div>
+        <div class="invoice-to">
+          <ul>
+            <li>
+              <h3>Invoce To:</h3>
+            </li>
+            <li>
+              <p>Alex Roy</p>
+            </li>
+            <li>
+              <p>93483759543</p>
+            </li>
+
+            <li>
+              <p>Dammam, Khubar</p>
+            </li>
+
+            <li>
+              <p>123, King Abdulaziz Road.</p>
+            </li>
+          </ul>
         </div>
-        <div class="col-6 text-end">
-          <h3>Invoice No: </h3>
-          <strong>23411</strong>
-          <br>
-          <small>Date: 12th December, 2022</small>
-          <br>
-          <small>Payment Status: Paid</small>
+        <div class="invoce-no">
+          <ul>
+            <li>
+              <h3>Invoce No:</h3>
+            </li>
+            <li>
+              <p>23411</p>
+            </li>
+            <li>
+              <p>93483759543</p>
+            </li>
+
+            <li>
+              <p>12th December, 2022</p>
+            </li>
+
+            <li>
+              <p>Payment Status: Paid</p>
+            </li>
+          </ul>
         </div>
       </div>
     </section>
@@ -64,107 +161,98 @@
       <div class="container">
         <div class="row">
           <div class="col-12">
-            <div class="product-item-tb">
-              <table class="table ">
-                <thead class="table-dark">
-                  <th>#</th>
-                  <th>Product Name</th>
-                  <th>Size</th>
-                  <th>Color</th>
-                  <th>Unit Price</th>
-                  <th>Quantity</th>
-                  <th width="120">Total</th>
-                </thead>
-                <tbody class="table-light">
-                  <tr>
-                    <td>1</td>
-                    <td>Mens Black T-shirt Casual</td>
-                    <td>sm</td>
-                    <td>Red</td>
-                    <td>50</td>
-                    <td>2</td>
-                    <td>100</td>
-                  </tr>
-                  <tr>
-                    <td>2</td>
-                    <td>women's T-shirt Casual</td>
-                    <td>md</td>
-                    <td>Green</td>
-                    <td>120</td>
-                    <td>1</td>
-                    <td>120</td>
-                  </tr>
-                  <tr>
-                    <td>3</td>
-                    <td>women's Toper Coat Casual</td>
-                    <td>md</td>
-                    <td>Black</td>
-                    <td>250</td>
-                    <td>1</td>
-                    <td>250</td>
-                  </tr>
-                  <tr>
-                    <td>4</td>
-                    <td>Mens Casual Print Shart</td>
-                    <td>lg</td>
-                    <td>Navi-blue</td>
-                    <td>150</td>
-                    <td>2</td>
-                    <td>300</td>
-                  </tr>
-                  <tr>
-                    <td>5</td>
-                    <td>Kids Toys Car</td>
-                    <td>null</td>
-                    <td>null</td>
-                    <td>50</td>
-                    <td>1</td>
-                    <td>50</td>
-                  </tr>
-                </tbody>
-              </table>
-
-              <div class="row">
-                <div class="col-4 ms-auto">
-                  <div class="row my-2">
-                    <div class="col-8 text-end">
-                      Subtotal
-                    </div>
-                    <div class="col-4">
-                      <span>SAR 2,250</span>
-                    </div>
-                  </div>
-
-                  <div class="row my-2">
-                    <div class="col-8 text-end">
-                      Shipping Cost
-                    </div>
-                    <div class="col-4">
-                      <span>SAR 25</span>
-                    </div>
-                  </div>
-
-                  <div class="row my-2">
-                    <div class="col-8 text-end">
-                      Tax (10%)
-                    </div>
-                    <div class="col-4">
-                      <span>SAR 25</span>
-                    </div>
-                  </div>
-
-                  <div class="row my-2 align-items-center bgc-primary-l3 p-2">
-                    <div class="col-8 text-end">
-                      Total Amount
-                    </div>
-                    <div class="col-4">
-                      <span>SAR 2,475</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <table class="table">
+              <thead class="table-dark">
+                <th>#</th>
+                <th style="text-align: left;">Product Name</th>
+                <th style="text-align: center;">Size</th>
+                <th style="text-align: center;">Color</th>
+                <th style="text-align: center;">Unit Price</th>
+                <th style="text-align: center;">Quantity</th>
+                <th style="text-align: center;">Total</th>
+              </thead>
+              <tbody class="table-light">
+                <tr>
+                  <td>1</td>
+                  <td style="text-align: left;">Mens Black T-shirt Casual</td>
+                  <td>sm</td>
+                  <td>Red</td>
+                  <td>50</td>
+                  <td>2</td>
+                  <td>100</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td style="text-align: left;">women's T-shirt Casual</td>
+                  <td>md</td>
+                  <td>Green</td>
+                  <td>120</td>
+                  <td>1</td>
+                  <td>120</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td style="text-align: left;">women's Toper Coat Casual</td>
+                  <td>md</td>
+                  <td>Black</td>
+                  <td>250</td>
+                  <td>1</td>
+                  <td>250</td>
+                </tr>
+                <tr>
+                  <td>4</td>
+                  <td style="text-align: left;">Mens Casual Print Shart</td>
+                  <td>lg</td>
+                  <td>Navi-blue</td>
+                  <td>150</td>
+                  <td>2</td>
+                  <td>300</td>
+                </tr>
+                <tr>
+                  <td>5</td>
+                  <td style="text-align: left;">Kids Toys Car</td>
+                  <td>null</td>
+                  <td>null</td>
+                  <td>50</td>
+                  <td>1</td>
+                  <td>50</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
+        </div>
+        <div class="row">
+          <table class="calculation">
+            <tr>
+              <td style="width:180px; text-align: left;">
+                <h4>Subtotal</h4>
+              </td>
+              <td>
+                <h4>SAR 2,250</h4>
+              </td>
+            </tr>
+            <tr>
+              <td style="width:180px; text-align: left;">Shippign Cost</td>
+              <td><span>SAR 30</span></td>
+            </tr>
+            <tr>
+              <td style="width:180px; text-align: left;">Discount Amount</td>
+              <td><span>SAR 250</span></td>
+            </tr>
+
+            <tr>
+              <td style="width:180px; text-align: left;">Tex</td>
+              <td><span>15%</span></td>
+            </tr>
+            <tr>
+              <td style="width:180px; text-align: left;">
+                <h3>Total</h3>
+              </td>
+              <td>
+                <h3>SAR 2,250</h3>
+              </td>
+            </tr>
+          </table>
         </div>
       </div>
     </section>
