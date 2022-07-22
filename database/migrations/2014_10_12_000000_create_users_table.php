8f0384d2a14082a917c6406696a9ed63b8bd0331
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-07-22 11:14:24 +0600

    update social login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,38 +6,38 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->text('avatar')->nullable();
-            $table->enum('role', ['Admin', 'Seller', 'Customer'])->default('Customer');
-            $table->string('phone_1')->nullable();
-            $table->string('phone_2')->nullable();
-            $table->decimal('balance', 10, 2)->default(0.00);
-            $table->decimal('due_balance', 10, 2)->default(0.00);
-            $table->text('address')->nullable();
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('users', function (Blueprint $table) {
+      $table->id();
+      $table->string('name');
+      $table->string('email')->unique();
+      $table->timestamp('email_verified_at')->nullable();
+      $table->string('password');
+      $table->text('avatar')->nullable();
+      $table->enum('role', ['Admin', 'Seller', 'Customer'])->default('Customer');
+      $table->string('phone_1')->nullable();
+      $table->string('phone_2')->nullable();
+      $table->decimal('balance', 10, 2)->default(0.00);
+      $table->decimal('due_balance', 10, 2)->default(0.00);
+      $table->text('address')->nullable();
+      $table->rememberToken();
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('users');
+  }
 }
