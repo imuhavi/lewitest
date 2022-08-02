75b359c0bd9d8fc48409900292c540cab13bfa07
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-08-03 01:57:52 +0600

    socail login

--- config/services.php
+++ config/services.php
@@ -33,12 +33,12 @@ return [
   'google' => [
     'client_id' => env('GOOGLE_CLIENT_ID'),
     'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-    'redirect' => 'http://127.0.0.1:8000/login/google/callback',
+    'redirect' => 'https://5dots.sa/login/google/callback',
   ],
 
   'github' => [
     'client_id' => env('GITHUB_CLIENT_ID'),
     'client_secret' => env('GITHUB_CLIENT_SECRET'),
-    'redirect' => 'http://127.0.0.1:8000/login/github/callback',
+    'redirect' => 'httpa://5dots.sa/login/github/callback',
   ],
 ];
