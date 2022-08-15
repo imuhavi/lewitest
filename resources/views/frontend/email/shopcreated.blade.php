0f877cc42d94c7daaaded2851470e848ae844466
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-08-15 18:42:42 +0600

    update shop

--- resources/views/frontend/email/shopcreated.blade.php
+++ resources/views/frontend/email/shopcreated.blade.php
@@ -1,26 +1,48 @@
-<!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Registraion</title>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title></title>
+  <link href='https://fonts.googleapis.com/css?family=Lato:300,400|Montserrat:700' rel='stylesheet' type='text/css'>
+  <style>
+    @import url(//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.1/normalize.min.css);
+  </style>
+  <link rel="stylesheet" href="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/default_thank_you.css">
+  <script src="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/jquery-1.9.1.min.js"></script>
+  <script src="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/html5shiv.js"></script>
+  <script src="https://kit.fontawesome.com/b868f71921.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-  <header>
-    <div class="container">
-      <div class="hedaer">
-        <h3>Shop Created</h3>
-      </div>
-    </div>
+  <header class="site-header" id="header">
+    <h1 class="site-header__title" data-lead-id="site-header-title">THANK YOU!</h1>
   </header>
+  <div class="main-content">
+    <i class="fa fa-check main-content__checkmark" id="checkmark"></i>
+    <p class="main-content__body" data-lead-id="main-content-body">We are glad that you have registered to our package.
+      Your Registraion is done. Please contact with Admin to active your shop as soon as.</p>
+  </div>
 
-  <main>
-    <h1>Shop Created</h1>
-  </main>
+  <div>
+    <h4>Package Details:</h4>
+    <p>Package Name: {{ $shop->shop->subscription->name }} ({{ $shop->shop->subscription->days
+      }} Days)</p>
+  </div>
 
+  <div>
+    <h4>Contact Info: </h4>
+    <p>Mobile Number: +966 53 458 8012</p>
+    <p>Email: support@5dots.com</p>
+  </div>
+
+  <footer class="footer" id="footer">
+    <p class="text-center">Copyright &copy
+      <script type="text/javascript"> document.write(new Date().getFullYear());</script> [5dots.sa] All
+      rights reserved.
+    </p>
+  </footer>
 </body>
 
 </html>
